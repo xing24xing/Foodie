1efca16c9c6674c9ce5cfc22779e4d89ac6b7f9d
xing24xing <khushipandey260304@gmail.com>
2024-12-14 01:11:44 +0530

    fig

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import SubHeading from './SubHeading/SubHeading.jsx';
 import Newsletter from './Footer/Newsletter.jsx';
 import FooterOverlay from './Footer/FooterOverlay.jsx';
-import MenuItem from './MenuItem/MenuItem.jsx';
+import MenuItem from './Menu/MenuItem.jsx';
 import Navbar from './Navbar/Navbar.jsx';
 
 export {

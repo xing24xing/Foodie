05d6bb0d0cd6170c57e1cfb07efe32c14a5d92d6
xing24xing <khushipandey260304@gmail.com>
2024-12-14 01:06:04 +0530

    deployed

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-import SubHeading from './SubHeading/SubHeading';
-import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './MenuItem/MenuItem';
-import Navbar from './Navbar/Navbar';
+import SubHeading from './SubHeading/SubHeading.jsx';
+import Newsletter from './Footer/Newsletter.jsx';
+import FooterOverlay from './Footer/FooterOverlay.jsx';
+import MenuItem from './MenuItem/MenuItem.jsx';
+import Navbar from './Navbar/Navbar.jsx';
 
 export {
   SubHeading,

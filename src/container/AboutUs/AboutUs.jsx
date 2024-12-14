6cca50df5159510ce9dd4c802d77552fca13d6dd
xing24xing <khushipandey260304@gmail.com>
2024-12-14 20:11:01 +0530

    adjust img

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className="app__aboutus app__bg flex__center section__padding" id="about">
     <div className="app__aboutus-overlay flex__center">
-      <img src={images.fc} alt="G_overlay" />
+      <img src={images.fc} alt="F_overlay" />
     </div>
 
     <div className="app__aboutus-content flex__center">
@@ -23,7 +23,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Founded in 2010, FoodieCapital started as a small, family-run restaurant with a big vision: to create a place where food brings people together. Over the years, we've grown into a beloved local gem, known for our innovative menus and impeccable service. Our history is rooted in passion, and we’re dedicated to serving delicious meals that will make you feel right at home.</p>
+        <p className="p__opensans">Founded in 2004, FoodieCapital started as a small, family-run restaurant with a big vision: to create a place where food brings people together. Over the years, we've grown into a beloved local gem, known for our innovative menus and impeccable service. Our history is rooted in passion, and we’re dedicated to serving delicious meals that will make you feel right at home.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>

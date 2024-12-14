28475c22685c52d8c85088741e5bc4af0885002a
xing24xing <khushipandey260304@gmail.com>
2024-12-14 20:15:04 +0530

    fix footer and findup content

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">Bengaluru, Karnataka, India</p>
+        <p className="p__opensans">+91 98765 43210</p>
+        <p className="p__opensans">+91 98765 43211</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -32,9 +32,9 @@ const Footer = () => (
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">09:00 am - 11:00 pm</p>
         <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans">08:00 am - 10:00 pm</p>
       </div>
     </div>
 
